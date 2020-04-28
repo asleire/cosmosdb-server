be2a74d57ea0efb0bd0cd3b8cd81e912754511be
Aleksander Sleire <alesle@signicat.com>
2020-04-28 16:24:32 +0200

    Return some empty objects from meta endpoint
    Otherwise Cosmos C# SDK will throw null exceptions

--- src/handler/read-meta.ts
+++ src/handler/read-meta.ts
@@ -1,1 +1,10 @@
-export default () => ({});
+export default () => ({ 
+    userConsistencyPolicy: {
+    },
+    userReplicationPolicy: {
+    },
+    systemReplicationPolicy: {
+    },
+    readPolicy: {
+    }
+});
